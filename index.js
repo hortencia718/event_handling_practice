9b4d3d9eecce25015b696c3d9bec681da07a5925
hortencia cisneros <hortenciaecisneros@gmail.com>
2021-07-06 16:43:04 -0400

    javascript for button

--- index.js
+++ index.js
@@ -2,26 +2,46 @@
 console.log("basic javascript");
 
 
-// find the appropriate element
-// attach an event listener on that element
-// tell the element/ code what to do once that event occurs
+// // find the appropriate element
+// // attach an event listener on that element
+// // tell the element/ code what to do once that event occurs
 
-// let tacoButton = document.querySelector("#taco")
+// // let tacoButton = document.querySelector("#taco")
 
-const linkName = document.querySelector(".link-name");
-const nameInput = document.querySelector(".name-input");
+// const linkName = document.querySelector(".link-name");
+// const nameInput = document.querySelector(".name-input");
 
 
-function setName(name){
-    linkName.innerText= "Welcome, ";
-    let nameInner =document.createElement("span");
-    nameInner.innerText = name;
-    nameInner.setAttribute("class", "name");
-    linkName.appendChild(nameInner);
-    return nameInner;
+// function setName(name){
+//     linkName.innerText= "Welcome, ";
+//     let nameInner =document.createElement("span");
+//     nameInner.innerText = name;
+//     nameInner.setAttribute("class", "name");
+//     linkName.appendChild(nameInner);
+//     return nameInner;
 
+// }
+
+// nameInput.addEventListener("change" , () => {
+//     setName(nameInput.value);
+// })
+
+
+
+const btn = document.querySelector(".btn");
+const text = document.querySelector('.text');
+
+let shown =true;
+
+// to toggle btn
+function toggleText(){
+    if(shown){
+        btn.innerText ="show text 👀";
+        text.getElementsByClassName.opacity =0;
+
+    }
+    else {
+        btn.innerText = "hide text 🙈";
+        text.getElementsByClassName.opacity = 1;
+    }
 }
-
-nameInput.addEventListener("change" , () => {
-    setName(nameInput.value);
-})
